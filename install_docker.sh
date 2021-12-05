89e9ef2485e7cd9767bcad5bc6ee11758f596530
jaccas <jaccas@gmail.com>
2021-12-05 22:40:03 +0000

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+function disconnected {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
 
-curl -sSL https://get.docker.com | sh || "Failed to install Docker!"
-sudo usermod -aG docker $USER || "Failed to add user to the Docker usergroup!"
+function check_online() {
+  printf "Checking online..."
+    wget -q --spider http://github.com
+    if [ $? -eq 0 ]; then
+    echo "Online."
+  else
+    error "You are Offline."
+  fi
+}
+
+check_online
+
+
+curl -sSL https://get.docker.com | sh || disconnected "Failed to install Docker!"
+sudo usermod -aG docker $USER || disconnected "Failed to add user to the Docker usergroup!"
 echo "Please reboot for take effect."
